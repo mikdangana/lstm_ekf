64a6ab32ac2f49c1f71cf2bf5f17f9e4b9236797
Ubuntu <ubuntu@ip-172-31-11-11.us-west-2.compute.internal>
2018-09-19 11:36:29 +0000

    Initial commit: tensorflow, EKF, source proc stats

---
+++ lstm_ekf.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import os
+
+def build_dataset():
+    proc = os.popen('top -b -n 2 | grep -v Tasks').read()
+    print(proc)
+    print(len(proc.split("\n")))
+
+def RNN(x, weights, biases):
+
+    # reshape to [1, n_input]
+    x = tf.reshape(x, [-1, n_input])
+
+    # Generate a n_input-element sequence of inputs
+    # (eg. [had] [a] [general] -> [20] [6] [33])
+    x = tf.split(x,n_input,1)
+
+    # 1-layer LSTM with n_hidden units.
+    rnn_cell = rnn.BasicLSTMCell(n_hidden)
+
+    # generate prediction
+    outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+
+    # there are n_input outputs but
+    # we only want the last output
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
+
+vocab_size = 1
+n_input = 3
+# number of units in RNN cell
+n_hidden = 512
+# RNN output node weights and biases
+weights = {
+    'out': tf.Variable(tf.random_normal([n_hidden, vocab_size]))
+}
+biases = {
+    'out': tf.Variable(tf.random_normal([vocab_size]))
+}
+
+
+build_dataset()
