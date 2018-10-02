7f357e1ff7c2452361959295edf343bbac93de97
Ubuntu <ubuntu@ip-172-31-11-11.us-west-2.compute.internal>
2018-10-02 11:49:40 +0000

    Convert raw data to float32, remove TIME+ column for now

--- lstm_ekf.py
+++ lstm_ekf.py
@@ -1,31 +1,37 @@
 import tensorflow as tf
 import os
-from numpy import resize, zeros
+from numpy import resize, zeros, float32
 
-n_input = 9
+n_input = 8
 
 def build_dataset():
-    cmd = "top -b -n 2 | grep -v Tasks | grep -v top | grep -v %Cpu | grep -v KiB | grep -v PID | grep [0-9] | awk '{print $1,$3,$4,$5,$6,$7,$9,$10,$11}'"
+    cmd = "top -b -n 2 | grep -v Tasks | grep -v top | grep -v %Cpu | grep -v KiB | grep -v PID | grep [0-9] | awk '{print $1,$3,$4,$5,$6,$7,$9,$10}'"
     pstats = os.popen(cmd).read()
     print(pstats)
     print(len(pstats.split()))
-    x = tf.Variable( resize(pstats.split(), (250,n_input)), name="inputs" )
+    pstatsf = list(map(lambda i: float32(i) if i != "rt" else 0, pstats.split()))
+    print("type = " + str(type(pstatsf[0])))
+    x = tf.Variable( resize(pstatsf, (250,n_input)), name="inputs" )
     return x
 
 def RNN(x, weights, biases):
 
     # reshape to [1, n_input]
     x = tf.reshape(x, [-1, n_input])
+    print("RNN.x = " + str(x))
 
     # Generate a n_input-element sequence of inputs
     # (eg. [had] [a] [general] -> [20] [6] [33])
     x = tf.split(x,n_input,1)
+    print("RNN.x1 = " + str(x))
 
     # 1-layer LSTM with n_hidden units.
     rnn_cell = tf.contrib.rnn.BasicLSTMCell(n_hidden)
+    print("RNN.rnn_cell = " + str(rnn_cell))
 
     # generate prediction
     outputs, states = tf.contrib.rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+    print("RNN.x3 = " + str(x))
 
     # there are n_input outputs but
     # we only want the last output
@@ -49,13 +55,14 @@ biases = {
     'out': tf.Variable(tf.random_normal([vocab_size]))
 }
 
+
 pred = RNN(x, weights, biases)
 
 # TODO: implement EKF-based cost function
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(cost)
+#cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+#optimizer = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(cost)
 
-y = zeros([vocab_size], dtype=float)
+#y = zeros([vocab_size], dtype=float)
 
-_, acc, loss, onehot_pred = tf.Session.run([optimizer, accuracy, cost, pred], feed_dict={x: x, y: y})
+#_, acc, loss, onehot_pred = tf.Session.run([optimizer, accuracy, cost, pred], feed_dict={x: x, y: y})
 
