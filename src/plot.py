7ef69f022f7e9fd69057c885b7ee4c9b7b872a18
Ubuntu <ubuntu@ip-172-31-11-11.us-west-2.compute.internal>
2019-07-26 11:21:29 +0000

    Plot_all, plotmulti, errorbars

--- src/plot.py
+++ src/plot.py
@@ -1,12 +1,15 @@
 from pylab import *
-from numpy import array, gradient, mean, std
+from numpy import array, gradient, mean, std, log
+import numpy as np
 from scipy import stats
-from math import log
+#from math import log
 from getopt import getopt, GetoptError
 from pandas import DataFrame
 from surface3d import plotsurface
 from utils import *
-import pickle, re, os
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import pickle, re
 import pylab
 
 
@@ -34,15 +37,18 @@ def loadfiles(filenames):
 
 def parse_client_info(filename):
     txt = pickleload(filename)
+    if not isinstance(txt, str):
+        return []
     toks = list(filter(lambda s: s.endswith("s"), re.split("[\s]+", txt)))[2:]
-    return [float(i.replace("s","")) for i in toks] 
+    return [float(i.replace("s","")) for i in toks]
 
- 
+
 def parse_string(s):
     rows = filter(lambda l: "fetch" not in l and len(l), s.split("\n"))
     rows = [re.compile(r'\W+').split(r) for r in rows]
     rows = [[r[0], r[1] + "." + r[2]] for r in rows]
     return [[float(c.replace("s","")) for c in r] for r in rows[1:]]
+
 
 
 def showplot():
@@ -80,7 +86,16 @@ def plotpredictions(args):
 def formatline(data, isGradient, indices, intervals, normalize):
     res = []
     for n in range(len(data)):
+        if '--log' in sys.argv:
+            data[n] = log(data[n])
         d = formatlines(data, isGradient, indices,intervals,normalize,n)
+        if not len(d):
+            continue
+        print("formatline.d.shape,n=" + str((len(shape(d[0])),n)))
+        if not len(indices):
+            for i in range(len(shape(d[0]))):
+                d = [x[0] for x in d]
+        print("formatline.d,shape = " + str((d, shape(d))))
         res = res + d if len(indices) else res + [d]
     if '--mean' in sys.argv:
         res.append([mean(data[0][0:i+1]) for i in range(len(data[0]))])
@@ -95,13 +110,16 @@ def formatlines(data, isGradient, indices, intervals, normalize, n):
         elif len(shape(data[n])) > 1:
             data[n] = list(filter(lambda r: len(shape(r)), data[n]))
             data[n] = [[r[int(i)] for r in data[n]] for i in indices]
-            print("formatline.data[n] = " +str(data[n][0:min(len(data[n]),10)]))
-    elif (len(data[n]) and len(shape(data[n])) > 1): 
-        data[n] = [r[-1] for r in data[n]]
+            print("formatlines.data[n] =" +str(data[n][0:min(len(data[n]),10)]))
+    #elif (len(data[n]) and len(shape(data[n])) > 1): 
+     #   data[n] = [r[-1] for r in data[n]]
 
+    print("formatlines.intervals,len = " + str((intervals, len(intervals))))
     if len(intervals):
-        (start, end) = intervals[0].split(":")
+        intervals = intervals[0] if len(intervals)>1 else intervals 
+        (start, end) = intervals.split(":")
         data[n] = data[n][int(start):int(end)]
+        print("formatlines.data.intervals.len = " + str(len(data[n])))
 
     if normalize:
         (maxd, mind) = (max([max(d) for d in data]),min([min(d) for d in data]))
@@ -109,44 +127,9 @@ def formatlines(data, isGradient, indices, intervals, normalize, n):
     return data[n]
 
 
-def plot_all(path):
-    global fig_file
-    print("plot_all().path = " + path)
-    os_run("tar xvf " + path + "/*.tar")
-    (data, count) = ({}, 0)
-    for r, d, f in os.walk(path):
-        for file in f:
-            fname = os.path.join(r, file)
-            if 'clientout.pickle' in file:
-                data[file] = parse_client_info(fname)
-            elif 'measurements.pickle' in file:
-                data[file] = col_data(fname, 0)
-                count = 1 if not count else count
-        for dir in d:
-            if 'run_' in dir:
-                datum = plot_all(os.path.join(r, dir))
-                data = {k:v + data[k] for k,v in datum.items()}
-                count = count + 1
-    data = {k:array(v)/count for k,v in data.items()}
-    for file in data.keys():
-        fname = os.path.join(path, file)
-        fig_file = fname.replace(".pickle", ".png")
-        plotlines(to_line(fname, data[file]))
-    print("plot_all() done")
-    return data
-                
-
-def col_data(path, index):
-    data = pickleload(path)
-    return [row[index] for row in data] if len(array(data).shape)>2 else data
-
-
-def to_line(fname, col):
-    pickledump(fname, col)
-    return [fname]
-
 
 def parse_line_args(args):
+    print("parse_line_args().args = " + str(args))
     for k in ["--xaxis", "--yaxis", "--title"]:
         if k in args:
             i = args.index(k)
@@ -155,7 +138,9 @@ def parse_line_args(args):
     normalize = "--normalize" in args or "-n" in args
     intervals = list(filter(lambda f: ":" in f, args))
     indices = list(filter(lambda f : f.isdigit(), args))
-    filenames = list(filter(lambda f : not f.isdigit() and not f.startswith("-") and not ":" in f, args))
+    (yerr, args) = parse_err_args(args, indices)
+    isfile =lambda f: not f.isdigit() and not f.startswith("-") and not ":" in f
+    filenames = list(filter(isfile, args))
     data = loadfiles(filenames)
     if not len(data):
         return (None, None, isGradient)
@@ -163,7 +148,20 @@ def parse_line_args(args):
     if isinstance(data[0], type("")):
         data = [parse_string(data[0])]
     data = formatline(data, isGradient, indices, intervals, normalize)
-    return (data, filenames, isGradient)
+    return (data, filenames, isGradient, yerr)
+
+
+def parse_err_args(args, indices):
+    if "--err" in args:
+        i = args.index("--err")
+        (data, args) = ([pickleload(args[i+1])], args[0:i] + args[i+2:])
+        if len(indices):
+            if len(indices)==1:
+                data = [[r[int(indices[0])] for r in f] for f in data]
+            else:
+                data = [[[r[int(i)] for i in indices] for r in f] for f in data]
+        return (data, args)
+    return (None, args)
 
 
 def parse_title(fname):
@@ -191,7 +189,7 @@ def printstats(prefix, data):
 
 
 def plotlines(args):
-    (data, filenames, isGradient) = parse_line_args(args)
+    (data, filenames, isGradient, yerr) = parse_line_args(args)
     if not data:
          return
     printstats("plotlines()", data)
@@ -199,9 +197,9 @@ def plotlines(args):
     xlabel(nextv(args, '--xaxis', 'Interval'))
     if len(filenames) <= 1:
         if '--vs' in args:
-            plot(data[0], data[1], 'r--')
+            plot(data[0], data[1], 'b', linewidth=1)
         else:
-            plot(arange(0, len(data[0]), 1), data[0], 'r--')
+            plot(arange(0, len(data[0]), 1), data[0], 'b', linewidth=1)
         ylabel(nextv(args, '--yaxis', lgd[filenames[0]] + '(red)'))
         title(nextv(args, '--title', lgd[filenames[0]] + ' vs Time'))
     else:
@@ -285,6 +283,123 @@ def plotscatter(filenames):
     showplot()
 
 
+def plot_all(path):
+    global fig_file
+    path = (path[0] if len(path) else "") if isinstance(path, list) else path
+    print("plot_all().path = " + path)
+    (data, maxs, mins, count) = ({}, {}, {}, 0)
+    for r, d, f in os.walk(path):
+        datum = {}
+        for file in f:
+            fname = os.path.join(r, file)
+            if 'clientout.pickle' in file:
+                datum[file] = parse_client_info(fname)
+            elif 'measurements.pickle' in file:
+                datum[file] = col_data(fname, 0)
+        for dir in d:
+            if 'run_' in dir:
+                datum = plot_all(os.path.join(r, dir))
+        if len(datum.items()):
+            items = datum.items()
+            data = {k:addm(v, data[k] if k in data else []) for k,v in items}
+            maxs = {k:maxv(v, data[k] if k in data else []) for k,v in items}
+            mins = {k:minv(v, data[k] if k in data else []) for k,v in items}
+            count = count + 1
+    data = {k:array(v)/count for k,v in data.items()}
+    for file in data.keys():
+        fname = os.path.join(path, file)
+        fig_file = fname.replace(".pickle", ".png")
+        yerr = subm(maxs[file], mins[file]) if file in maxs else []
+        if file in maxs:
+            print("plot_all().max,min,yerr = "+str((file, len(data[file]),len(maxs[file]),len(mins[file]),len(yerr))))
+        plotmulti(to_line(fname, data[file], yerr))
+    print("plot_all() done")
+    return data
+
+
+def col_data(path, index):
+    data = pickleload(path)
+    return [row[index] for row in data] if len(array(data).shape)>2 else data
+
+
+def iscollection(vs, i):
+    v = vs[i] if len(vs)>i else None
+    return isinstance(v, list) or isinstance(v, type(array([])))
+
+
+def addm(v1, v2):
+    (a, b) = (v1, v2) if len(v1) >= len(v2) else (v2, v1)
+    addfn = lambda v,w: addm(v, w) if iscollection(v1, 0) else v + w
+    return [addfn(v,(v if i>=len(b) else b[i])) for i,v in zip(range(len(a)), a)]
+
+
+def subm(v1, v2):
+    (a, b) = (v1, v2) if len(v1) >= len(v2) else (v2, v1)
+    subfn = lambda v,w: subm(v,w) if iscollection(v1, 0) else v - w
+    return [subfn(v,(v if i>=len(b) else b[i])) for i,v in zip(range(len(a)), a)]
+
+
+def maxv(v1, v2):
+    (a, b) = (v1, v2) if len(v1) >= len(v2) else (v2, v1)
+    maxfn = lambda v,w: maxv(v,w) if iscollection(v1, 0) else max(v, w)
+    return [maxfn(v, v if i>=len(b) else b[i]) for i,v in zip(range(len(a)), a)]
+
+
+def minv(v1, v2):
+    (a, b) = (v1, v2) if len(v1) >= len(v2) else (v2, v1)
+    minfn = lambda v,w: minv(v,w) if iscollection(v1, 0) else min(v, w)
+    return [minfn(v, v if i>=len(b) else b[i]) for i,v in zip(range(len(a)), a)]
+
+
+def to_line(fname, col, yerr):
+    pickledump(fname, col)
+    if yerr and len(yerr):
+        efile = fname.replace(".pickle", "_err.pickle")
+        pickledump(efile, yerr)
+        args = [fname, "--err", efile, "0"]
+    else:
+        args = [fname]
+    return args
+
+
+def trim_axs(axs, N):
+    """little helper to massage the axs list to have correct length..."""
+    axs = axs.flat
+    for ax in axs[N:]:
+        ax.remove()
+    return axs[:N]
+
+
+def plotmulti(args):
+    (data, filenames, isGradient, yerr) = parse_line_args(args)
+    if not data: # or len(filenames) <= 1:
+        return plotlines(args)
+    #printstats("plotmulti()", data)
+    (lgd, styles) = (legend(isGradient, filenames), ['r--', 'b','g','y'])
+    (x,n) = (data[0],1) if '--vs' in args else (arange(0, len(data[0]), 1),0)
+    (figsize, i, delta) = ((8,4), 0, 0.11)
+    (cols, rows) = (2, 1 if len(filenames)==1 else round(len(filenames)/2))
+    fig1, axs = plt.subplots(rows,cols,figsize=figsize, constrained_layout=True)
+    fig1.text(0.5, 0.04, nextv(args, '--xaxis', 'Interval'), ha='center')
+    fig1.text(0.04, 0.5, nextv(args, '--yaxis', 'Value'), va='center', rotation='vertical')
+    axs = trim_axs(axs, len(filenames))
+    for ax, y, file in zip(axs, data[n:], filenames):
+        if yerr:
+            ax.errorbar(x, y, every(yerr[filenames.index(file)], 10), solid_capstyle='projecting', capsize=5, ecolor='grey')
+        else:
+            ax.plot(x, y, '', ls='-', ms=4)
+        ax.set_title(nextv(args, '--title', 'Subplot ') + str(suffix(file)))
+    pylab.legend()
+    showplot() 
+
+
+def every(v, n):
+    return [v if i%n==0 else 0 for i,v in zip(range(len(v)), v)]
+
+
+def suffix(file):
+    return re.sub(r"[_\\.].*", "", file.split("_")[1]) if "_" in file else ""
+
 
 def usage():
     print("\nUsage: " + sys.argv[0] + " [-h | -s | -l | -p | -3] [file(s)]\n" +
@@ -305,7 +420,7 @@ def usage():
 
 def main():
     try:
-        opts, args = getopt(sys.argv[1:], "hslpa:3", ["help", "scatter", "line", "predictions", "surface3d", "all"])
+        opts, args = getopt(sys.argv[1:], "hslpa:3", ["help", "scatter", "line", "predictions", "surface3d", "all", "multi"])
     except GetoptError as err:
         print(str(err))
         usage()
@@ -320,8 +435,10 @@ def main():
             plotpredictions(arg)
         elif o in ( "-3", "--surface3d" ):
             plotsurface(arg)
-        elif o in ( "-a", "--plotall" ):
-            plot_all(a)
+        elif o in ( "-a", "--all" ):
+            plot_all(args)
+        elif o in ( "-m", "--multi" ):
+            plotmulti(arg)
         else:
             usage()
     if not len(opts):
